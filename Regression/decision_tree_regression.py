237998cd2a9976a71815579f980f9d71088de706
neubrom <epro050@t-online.de>
2021-12-30 11:27:43 +0100

    Day 4

--- Regression/decision_tree_regression.py
+++ Regression/decision_tree_regression.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the datasett
+# Importing the dataset
 dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
