7bfbab9f3adf85d8c148008eb99bf0599724a786
neubrom <epro050@t-online.de>
2022-01-02 22:47:22 +0100

    Day 4

--- Classification/kernel_svm.py
+++ Classification/kernel_svm.py
@@ -10,7 +10,7 @@ dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+# Spliting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
